8db17fb4c42e0a654a386f3a2e07f0a89ba1677c
ShushanNigoyan <90801339+ShushanNig@users.noreply.github.com>
2022-11-25 02:10:20 +0400

    2

--- main.tf
+++ main.tf
@@ -50,8 +50,8 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog_sg"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
-
+  vpc_id = module.blog_vpc.vpc_id.public_subnets[0]
+  
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
