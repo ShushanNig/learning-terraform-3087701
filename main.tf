a0d398323b3c84abe286f33c6e1b4e092e293965
ShushanNigoyan <90801339+ShushanNig@users.noreply.github.com>
2022-11-26 01:24:13 +0400

    autoscaling

--- main.tf
+++ main.tf
@@ -28,6 +28,20 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.3"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id          = data.aws_ami.app_ami.id
+  instance_type     = var.instance
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -60,7 +74,7 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"] 
 } 
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
