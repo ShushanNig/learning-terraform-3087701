6d19136e4e975751d1660a9359b0e523314b830c
ShushanNigoyan <90801339+ShushanNig@users.noreply.github.com>
2022-11-26 00:49:46 +0400

    123

--- main.tf
+++ main.tf
@@ -17,8 +17,9 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -50,7 +51,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog_sg"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
