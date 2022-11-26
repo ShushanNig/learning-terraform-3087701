418af359ea7e3b4c4f797b901d6c900a48b2d30f
ShushanNigoyan <90801339+ShushanNig@users.noreply.github.com>
2022-11-27 02:14:34 +0400

    3

--- qa/main.tf
+++ qa/main.tf
@@ -1,3 +1,11 @@
-module "dev" {
+module "qa" {
     source = "../modules/blog"
+
+    environment = {
+        name = "qa"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
 }
